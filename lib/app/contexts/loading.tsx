62d41b92dbdff03b522ef55c729f204fd98ef888
AminGhasempoor <aminghasempoor79@gmail.com>
2024-01-15 14:06:02 +0330

    loading page and restyle loading in row and banner component

---
+++ lib/app/contexts/loading.tsx
@@ -0,0 +1,34 @@
+import LoadingHardPage from "@/core/components/LoadingHardPage";
+import { createContext, ReactNode, useContext, useState } from "react";
+
+export const LoadingContext = createContext({
+  loadingPage: false,
+  setLoadingPage: (loading: boolean) => {},
+});
+
+export const LoadingProvider = ({ children }: { children: ReactNode }) => {
+  const [loadingPage, setLoadingPage] = useState(false);
+
+  return (
+    <LoadingContext.Provider value={{ loadingPage, setLoadingPage }}>
+      <LoaderWrapper>{children}</LoaderWrapper>
+    </LoadingContext.Provider>
+  );
+};
+
+const LoaderWrapper = ({ children }: { children: ReactNode }) => {
+  const { loadingPage } = useContext(LoadingContext);
+
+  return (
+    <LoadingHardPage
+      loading={loadingPage}
+      sx={{}} // Add your styles here
+      icon={null}
+      width={200}
+      height={200}
+      label="Loading ..."
+    >
+      {children}
+    </LoadingHardPage>
+  );
+};
